1a67292a63d8a68884c00e7cf244da2b98a189ad
Emil Wallner <w@llner.co>
2017-09-28 19:56:03 +0200

    nst

--- Full-version/resources/__init__.py
+++
@@ -1,4 +0,0 @@
-from .inception_resnet_v2 import inception_resnet_v2, \
-    inception_resnet_v2_arg_scope
-from .inception_utils import maybe_download_inception, \
-    prepare_image_for_inception, checkpoint_url
